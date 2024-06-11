15bcd91f95300a29e95d367268e1fb983b687a45
Jpeterson2012 <zerogravity@gmail.com>
2024-06-11 08:23:18 -0400

    .

--- backend/routes/search.js
+++ backend/routes/search.js
@@ -3,17 +3,28 @@ var router = express.Router();
 
 router.get('/:id', async (req, res) => {
     // console.log(req.params.id)
-    
-    url = `https://api.spotify.com/v1/search?q=${req.params.id}&type=track,artist,album,playlist`
     const headers = {
-        Authorization: 'Bearer ' + process.env.access_token
-      }
+      Authorization: 'Bearer ' + process.env.access_token
+    }
+    var i = 0
+    for (var i = 0; i < 3; i++){
+      url = `https://api.spotify.com/v1/search?q=${req.params.id}&offest=${i}&limit=1&type=track,artist,album,playlist`
+      var resp = await fetch(url, {headers})
+      var data = await resp.json()
+      res.write(data.albums.total.toString())
+    }
+    res.end()
+    
+    // url = `https://api.spotify.com/v1/search?q=${req.params.id}&type=track,artist,album,playlist`
+    // const headers = {
+    //     Authorization: 'Bearer ' + process.env.access_token
+    //   }
 
-    var resp = await fetch(url, {headers})
-    var data = await resp.json()
+    // var resp = await fetch(url, {headers})
+    // var data = await resp.json()
     
 
-    res.send(data)
+    // res.send(data)
     
     
 })
